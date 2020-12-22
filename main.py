7ff1ea581bb389fc856a21a02e1bed3010864630
stepech <st3pech@protonmail.com>
2020-12-22 22:25:09 +0100

    Reworked whole code

--- main.py
+++ main.py
@@ -1,47 +1,31 @@
 import requests
-from secretsu import get_url
-from secretsu import reformator
-
-def get_hook_info(url):
-
-    response = requests.get(url)
-    return response.text
 
 
-def send_message(url, msg):
-    payload = {
-        "username": "Oskar",
-        "content": msg,
-        "avatar_url": "https://i.imgur.com/Q5W52JU.png"
-    }
-    requests.post(url, json=payload)
+DISCORD_WEBSITE = "https://discord.com/api/webhooks/"
+
+def open_file(file):
+    try:
+        f = open(file, 'r')
+    except:
+        print('There is no file ' + file + ', do you want to create one? (Y/N)')
+        response = str(input("> "))
+        if response == "Y" or response == "y":
+            f = open(file, 'x')
+            f.close()
+            f = open(file, 'r')
+        else:
+            quit()
+    print("Opened", file)
+    return f
 
 
 def start():
+    urlsfile = open_file("urls.txt")
+    urls = {}
+    for line in urlsfile:
+        urls[line.rstrip()] = urlsfile.readline().rstrip()
 
-    print('Choose WebHook')
-    print('1 - Testing on klacek server')
-    print('2 - Sharp on klacek server')
-    print('3 - Sharp on Oskar server')
-
-    dec = int(input('Enter number 1/2/3> '))
-    url = get_url(dec)
-    while url == 0:
-        dec = int(input('Enter only number 1/2/3> '))
-        get_url(dec)
-
-    print('Url =', url)
-
-    stats = get_hook_info(url)
-    print(stats)
-
-    msg = "smth"
-    while msg != "":
-        print('\nEnter message, empty to end')
-        msg = str(input("> "))
-        if msg != "":
-            msg = reformator(msg)
-            send_message(url, msg)
+    print("Loading", len(urls), "bots...")
 
 
 if __name__ == '__main__':
